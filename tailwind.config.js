093e42f64be429e892fbc5c6da003f012872d4f7
azriel123123 <mazrielilham4@gmail.com>
2024-01-31 20:27:54 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       // membuat custom spacing
@@ -18,6 +19,8 @@ module.exports = {
       'Poppins' : ['Poppins']
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
